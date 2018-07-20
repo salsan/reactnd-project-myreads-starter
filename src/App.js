96e9a34c1a70b3e569b7983ada3854f75815051a
Salvatore Santagati <salvatore.santagati@gmail.com>
2018-07-20 13:49:18 +0200

    fix: resolve problem about duplicate title

--- src/App.js
+++ src/App.js
@@ -29,9 +29,6 @@ class BooksApp extends React.Component {
           }/>
           <Route exact path="/" render={() => (
             <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
               <BooksContent/>
               <div className="open-search">
                 <Link to="/search">Add a book</Link>
